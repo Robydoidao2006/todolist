694d638a0c68e4c74a1c609c5e44a3707ce54cd4
Robydoidao2006 <Robydoidao2006@yahoo.com>
2018-11-06 09:26:30 -0500

    new commit

---
+++ build/precache-manifest.f245119d7b6cee4b9eb54fe86f2e406b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "bb991d178358fa3be476",
+    "url": "/todolist/static/js/main.bb991d17.chunk.js"
+  },
+  {
+    "revision": "910c9652af037f81b001",
+    "url": "/todolist/static/js/1.910c9652.chunk.js"
+  },
+  {
+    "revision": "bb991d178358fa3be476",
+    "url": "/todolist/static/css/main.76b0f300.chunk.css"
+  },
+  {
+    "revision": "e5818e893220a8ffe4fd0631559c94f7",
+    "url": "/todolist/index.html"
+  }
+];
