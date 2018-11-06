d47cee94fdecfa2e3be08d673b06db6911ab335d
Robydoidao2006 <Robydoidao2006@yahoo.com>
2018-11-06 09:23:59 -0500

    Updates

---
+++ precache-manifest.b5695921741fd117c4125ae37868a98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "d4928dacbb4a40e3dc11",
+    "url": "/todolist/static/js/main.d4928dac.chunk.js"
+  },
+  {
+    "revision": "910c9652af037f81b001",
+    "url": "/todolist/static/js/1.910c9652.chunk.js"
+  },
+  {
+    "revision": "d4928dacbb4a40e3dc11",
+    "url": "/todolist/static/css/main.a915bf09.chunk.css"
+  },
+  {
+    "revision": "1ca070f159d26a0a3958d38ef729eea1",
+    "url": "/todolist/index.html"
+  }
+];
